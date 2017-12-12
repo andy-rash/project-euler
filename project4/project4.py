7a6bc584078c9dad5c1f056f0326082b08fa3772
Andy <andy.rash1996@gmail.com>
2017-12-12 11:07:57 -0600

    made project4 solution a one-liner

--- project4/project4.py
+++ project4/project4.py
@@ -1,2 +1,1 @@
-def p(x):return False if x==0 else str(x)==str(x)[::-1]
-print(max([x*y for x in range(999) for y in range(999) if p(x*y)]))
+print(max([x*y for x in range(999) for y in range(999) if x*y!=0 and str(x*y)==str(x*y)[::-1]]))
